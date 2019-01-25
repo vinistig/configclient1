2f7482815dbf7c95505f7ceaa8b361e287cb3809
Vinicius Stigliani <vinicius.stigliani@synchro.com.br>
2019-01-25 14:56:41 -0200

    small changes

--- src/main/java/com/example/configclient/controller/MainController.java
+++ src/main/java/com/example/configclient/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @RefreshScope
 @RestController
